5c3ce2b5f30b788f74c53b1fe9c76caf8a2f7003
Josh Wood <josh.wood@insight.com>
2020-07-21 09:56:17 -0400

    Add Lambda Proxy Integration
    Support integrating a lambda with apigw

---
+++ examples/api-gateway-lambda-integration/variables.tf
@@ -0,0 +1,15 @@
+variable "lambda-name" {
+    type = string
+}
+
+variable "api-gw-name" {
+    type = string
+}
+
+variable "http-action" {
+    type = string
+}
+
+variable "route" {
+    type = string
+}
